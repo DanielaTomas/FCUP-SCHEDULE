f3a1390b8a556322f7f4916962e109de41d40045
Daniela Tomás <daniela.stomas02@gmail.com>
2025-04-01 10:30:38 +0100

    add more tests

--- Docs/Tests/run.sh
+++ Docs/Tests/run.sh
@@ -12,3 +12,13 @@ echo pypy3 -u -mcts_input_parser.py --time_limit=3600 --c_param=1
 pypy3 -u mcts_input_parser.py --time_limit=3600 --c_param=1
 tar zcvf mcts_c1.tgz mcts_tree/ constraint_progress/ output/
 rm -rf mcts_tree/ constraint_progress/ output
+
+TMLIM=600
+for CPAR in 1000 500 200 100 50 20 10 5 2 1; do
+    for SEED in `seq -f "%02g" 1 10`; do
+        echo "C=$CPAR, seed=$SEED"
+        pypy3 -u mcts_input_parser.py --time_limit=$TMLIM --c_param=$CPAR --seed=$SEED
+        tar zcvf mcts_time-$TMLIM_C-$CPAR_$SEED.tgz mcts_tree/ constraint_progress/ output/
+        rm -rf mcts_tree/ constraint_progress/ output/
+    done
+done
